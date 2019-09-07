e32a9982f6163b474b0ec1b7bcdce7afba1eecc7
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-09-07 16:19:42 +0300

    CCP - finished development of slider

--- src/components/career/our_team_section/index.js
+++ src/components/career/our_team_section/index.js
@@ -11,7 +11,47 @@ const OurTeamSection = () => {
                         65 человек из 7 стран мира (РФ, Украины, РБ, Латвии, Италии, Испании и Швейцарии), - в ленте фотографии самых смелых из нас :)
                     </div>
                 </div>
-                <ImageSlider />
+                <ImageSlider
+                    items={
+                        [
+                            {
+                                imgSrc: '/ourTeamPhotos/women1.png',
+                                name: 'Лана Дэль Рэй',
+                                position: 'Певица'
+                            },
+                            {
+                                imgSrc: '/ourTeamPhotos/women2.png',
+                                name: 'Анджелина Джоли',
+                                position: 'Актриса'
+                            },
+                            {
+                                imgSrc: '/ourTeamPhotos/man2.png',
+                                name: 'Брэд Питт',
+                                position: 'Актер'
+                            },
+                            {
+                                imgSrc: '/ourTeamPhotos/man3.png',
+                                name: 'Крис Хемсворт',
+                                position: 'Актер'
+                            },
+                            {
+                                imgSrc: '/ourTeamPhotos/women4.png',
+                                name: 'Эмма Уотсон',
+                                position: 'Актриса'
+                            },
+                            {
+                                imgSrc: '/ourTeamPhotos/man4.png',
+                                name: 'Роберт Дауни(младший)',
+                                position: 'Актер'
+                            },
+                            {
+                                imgSrc: '/ourTeamPhotos/women3.png',
+                                name: 'Скарлетт Йоханссон',
+                                position: 'Актриса'
+                            }
+                        ]
+                    }
+                />
             </div>
         </div>
     );
