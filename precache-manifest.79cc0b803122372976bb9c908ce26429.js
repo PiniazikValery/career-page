af9e5af84ec65184631326ae5f5a4d279b69a03d
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-09-11 18:09:58 +0300

    Updates

--- precache-manifest.79cc0b803122372976bb9c908ce26429.js
+++ precache-manifest.79cc0b803122372976bb9c908ce26429.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0cd70ce85c67ed22801a9194dc205b19",
+    "revision": "aeac04ab2b3e1da54ed01572ba7cfd6a",
     "url": "/career-page/index.html"
   },
   {
-    "revision": "a4d5c7e69117e244220e",
-    "url": "/career-page/static/css/main.8a8e9776.chunk.css"
+    "revision": "71f0ead7ee24635e8319",
+    "url": "/career-page/static/css/main.1063474e.chunk.css"
   },
   {
     "revision": "67b303cf40758a902ed4",
     "url": "/career-page/static/js/2.7c96c62b.chunk.js"
   },
   {
-    "revision": "a4d5c7e69117e244220e",
-    "url": "/career-page/static/js/main.57e5b81b.chunk.js"
+    "revision": "71f0ead7ee24635e8319",
+    "url": "/career-page/static/js/main.c591c010.chunk.js"
   },
   {
     "revision": "7a3c954be10f956886c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/career-page/precache-manifest.123d662fb273844008404b172a92a4a9.js"
+  "/career-page/precache-manifest.79cc0b803122372976bb9c908ce26429.js"
 );
 
 self.addEventListener('message', (event) => {
