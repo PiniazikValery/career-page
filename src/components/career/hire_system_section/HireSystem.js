56941497e1b757fe290f6d1b82235f1b5eee84bd
Valery Piniazik <valery.piniazik@itechart-group.com>
2019-09-17 23:42:59 +0300

    DMHSS - mobile hire system has been developed

---
+++ src/components/career/hire_system_section/HireSystem.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Point, EmailIcon } from './icons';
+
+const HireSystem = (props) => {
+    return (
+        <div className={`hire-system-section${props.mobile ? ' mobile' : ''}`}>
+            <div className="hire-system-content">
+                <div className="intro">
+                    Система найма
+                </div>
+                <div className="line">
+                    <div className="send-point">
+                        <div className="point">
+                            <Point />
+                            <div className="point-content">
+                                <div className="intro">
+                                    ОТПРАВЛЯЕТЕ
+                            <div className="description">
+                                        анкету с кейсами или техническим заданием
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="check-point">
+                        <div className="point">
+                            <Point />
+                            <div className="point-content">
+                                <div className="intro">
+                                    ПРОВЕРЯЕМ
+                            <div className="description">
+                                        вашу работу в течение 7 дней
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="answer-point">
+                        <div className="point">
+                            <Point />
+                            <div className="point-content">
+                                <div className="intro">
+                                    ОТВЕЧАЕМ
+                            <div className="description">
+                                        либо приглашаем на собеседование в live-чат
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="finish-point"><EmailIcon /></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HireSystem;
