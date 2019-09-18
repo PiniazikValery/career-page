9daf69635f0f6cdfe3e276c63313d6fa051a294b
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-09-18 14:33:59 +0300

    DMAS - mobile alternatives section has been developed

--- src/responsive_design/index.js
+++ src/responsive_design/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Responsive from 'react-responsive';
-import deviseSizes from './device_sizes';
+import deviceSizes from './device_sizes';
 
-export const Mobile = props => <Responsive {...props} maxWidth={deviseSizes.mobileMaxWidth} />;
-export const Desktop = props => <Responsive {...props} minWidth={deviseSizes.desktopMinWidth} />;
+export const Mobile = props => <Responsive {...props} maxWidth={deviceSizes.mobileMaxWidth} />;
+export const Desktop = props => <Responsive {...props} minWidth={deviceSizes.desktopMinWidth} />;
