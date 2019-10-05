581d9250bec1c7227532827ae3f589df8f52c472
ICX\valery.piniazik <valery.piniazik@itechart-group.com>
2019-10-05 16:28:36 +0300

    DVP - vacancy description and vacancy title has been adopted to mobile design

--- src/components/career/vacancy_description_section/index.js
+++ src/components/career/vacancy_description_section/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { BackArrow, PinterestIcon, FacebookIcon, TwitterIcon, OptionPoint } from './icons';
+import React, { useState } from 'react';
+import { BackArrow, PinterestIcon, FacebookIcon, TwitterIcon, OptionPoint, ShareIcon, InstagramIcon } from './icons';
 import { HashLink as Link } from 'react-router-hash-link';
 import strings from '../pages/vacancy_page/page_content';
+import { useMediaQuery } from 'react-responsive';
+import deviceSizes from '../../../responsive_design/device_sizes';
+import { Mobile, Desktop } from '../../../responsive_design';
 
 const VacancyDescriptionSection = (props) => {
+    const isMobile = useMediaQuery({ query: `(max-width: ${deviceSizes.mobileMaxWidth}px)` });
+    const [mobileShareMenuIsOpened, openMobileShareMenu] = useState(false);
+
     const RenderBlocks = () => {
         return (strings[props.vacancy].listBlocks.map(block => (
             <div key={block.id} className="req-block">
@@ -17,8 +23,49 @@ const VacancyDescriptionSection = (props) => {
         )));
     }
 
+    const RenderMobileShareMenu = () => {
+        if (mobileShareMenuIsOpened) {
+            return ([
+                <div key="blackout" onClick={onClickBlackout} className="blackout" />
+                ,
+                <div key="mobile-share-menu" className="mobile-share-menu">
+                    <div className="title">
+                        {strings.share}
+                    </div>
+                    <div className="share-icon">
+                        <InstagramIcon className="icon" />
+                        <div className="icon-name">Instagram</div>
+                    </div>
+                    <div className="share-icon">
+                        <PinterestIcon className="icon" />
+                        <div className="icon-name">Pinterest</div>
+                    </div>
+                    <div className="share-icon">
+                        <FacebookIcon className="icon" />
+                        <div className="icon-name">Facebook</div>
+                    </div>
+                    <div className="share-icon">
+                        <TwitterIcon className="icon" />
+                        <div className="icon-name">Twitter</div>
+                    </div>
+                </div>
+            ]
+            );
+        }
+        return undefined;
+    };
+
+    const onClickMobileShareButton = () => {
+        openMobileShareMenu(true);
+    };
+
+    const onClickBlackout = () => {
+        openMobileShareMenu(false);
+    };
+
     return (
-        <div className="vacancy-description-section">
+        <div className={`vacancy-description-section${isMobile ? ' mobile' : ''}`}>
+            {RenderMobileShareMenu()}
             <div className="vacancy-description-content">
                 <div className="content-header">
                     <Link to="/home#vacancies">
@@ -27,12 +74,17 @@ const VacancyDescriptionSection = (props) => {
                             {strings.back_to_all_vacancies}
                         </div>
                     </Link>
-                    <FacebookIcon className="share-icon" />
-                    <PinterestIcon className="share-icon" />
-                    <TwitterIcon className="share-icon" />
-                    <div className="share-links">
-                        {strings.share}
-                    </div>
+                    <Desktop>
+                        <FacebookIcon className="share-icon" />
+                        <PinterestIcon className="share-icon" />
+                        <TwitterIcon className="share-icon" />
+                        <div className="share-links">
+                            {strings.share}
+                        </div>
+                    </Desktop>
+                    <Mobile>
+                        <ShareIcon onClick={onClickMobileShareButton} className="share-icon" />
+                    </Mobile>
                 </div>
                 <div className="description">{strings[props.vacancy].description}</div>
                 <div className="req-blocks">
--- src/components/career/vacancy_title_section/index.js
+++ src/components/career/vacancy_title_section/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import strings from '../pages/vacancy_page/page_content';
+import { useMediaQuery } from 'react-responsive';
+import deviceSizes from '../../../responsive_design/device_sizes';
 
 const VacancyTitleSection = (props) => {
+    const isMobile = useMediaQuery({ query: `(max-width: ${deviceSizes.mobileMaxWidth}px)` });
+
     return (
-        <div className="vacancy_title_section">
+        <div className={`vacancy_title_section${isMobile ? ' mobile' : ''}`}>
             <div className="map-background" style={{ backgroundImage: 'url("/map.png")' }} />
             <div className="vacancy-title">{strings[props.vacancy].name}</div>
             <div className="vacancy-salary">{strings[props.vacancy].salary}</div>
